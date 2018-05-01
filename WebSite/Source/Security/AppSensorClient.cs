d31e3f55abc0afbc0a1d7a5ce692256c5e7c1927
marcin93w <marcin93w@gmail.com>
2018-05-01 13:04:33 +0200

    Implemented AppSensor client

---
+++ WebSite/Source/Security/AppSensorClient.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Driver.WebSite.Source.Security
+{
+    public class AppSensorClient
+    {
+        private readonly HttpClient client;
+        private readonly JsonSerializerSettings JsonSerializerSettings;
+
+        public AppSensorClient()
+        {
+            client = new HttpClient();
+            client.DefaultRequestHeaders.Add("X-Appsensor-Client-Application-Name2", "myclientapp");
+            JsonSerializerSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+        }
+
+        public async Task ReportEventAsync(Event @event)
+        {
+            var json = JsonConvert.SerializeObject(@event, JsonSerializerSettings);
+
+            var stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("http://localhost:8085/api/v1.0/events", stringContent);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ WebSite/Source/Security/Event.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Driver.WebSite.Source.Security
+{
+    public class Event
+    {
+        public User User { set; get; }
+        public DetectionPoint DetectionPoint { set; get; }
+        public DateTime Timestamp { get; set; }
+
+        public Event()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+
+    public class User
+    {
+        public string Username { set; get; }
+    }
+
+    public class DetectionPoint
+    {
+        public string Label { set; get; }
+        public string Category { set; get; }
+    }
+}
